a5cd591a6b2e8da93b875cb9354dc20f427d963c
NiklasDimo <83953800+NiklasDimo@users.noreply.github.com>
2022-12-13 16:19:25 +0100

    Neuster Stand

--- src/main/java/com/example/ESEG/model/User.java
+++ src/main/java/com/example/ESEG/model/User.java
@@ -13,18 +13,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String username;
-
     private String password;
-
     private String role;
-
     private String discount;
+    private String vorname;
+    private String nachname;
 
-    public User(String username, String password, String role, String discount) {
+    public User(String username, String password, String role, String discount, String vorname, String nachname) {
         this.username = username;
         this.password = password;
         this.role = role;
         this.discount = discount;
+        this.vorname = vorname;
+        this.nachname = nachname;
     }
 
     public User(){
