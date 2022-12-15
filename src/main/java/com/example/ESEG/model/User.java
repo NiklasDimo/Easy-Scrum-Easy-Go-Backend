c7e80ac67f295a000f27dd362bdd9bb8e15bbeae
Sidonke <timkka@web.de>
2022-12-15 20:06:39 +0100

    added user firstName, lastName to user

--- src/main/java/com/example/ESEG/model/User.java
+++ src/main/java/com/example/ESEG/model/User.java
@@ -13,13 +13,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String username;
+
+    private String firstName;
+
+    private String lastName;
     private String password;
     private String role;
     private String discount;
 
 
-    public User(String username, String password, String role, String discount) {
+    public User(String username, String password, String role, String discount, String firstName, String lastName) {
         this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.password = password;
         this.role = role;
         this.discount = discount;
