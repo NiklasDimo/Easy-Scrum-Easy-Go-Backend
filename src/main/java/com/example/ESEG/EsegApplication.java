30c500a082d9e5034f4c639991bdd9edcebd7484
Sidonke <timkka@web.de>
2023-01-27 02:10:12 +0100

    Detail Infos geändert

--- src/main/java/com/example/ESEG/EsegApplication.java
+++ src/main/java/com/example/ESEG/EsegApplication.java
@@ -21,14 +21,14 @@ public class EsegApplication {
 			//add Product
 			impressumRepository.save(new Impressum(null, "Easy Scrum Easy Go", "Beethovenstraße 1", "73430 Aalen", "Telefon: +49 7361 576-0", "Telefax: +49 7361 576-2250", "E-Mail: info@hs-aalen.de"));
 			repository.save(new Product("Playstation 5", "€", 10.50, "Von Haus aus steckt in der PS5 eine SSD-Festplatte mit 825 Gigabyte Speicher. Sollte euch das nicht reichen, könnt ihr die Konsole per NVMe SSD Slot durch eine weitere Festplatte erweitern.", "Maße: 10x20", "Konsole"));
-			repository.save(new Product("Nintendo Switch", "€", 5.99, "Das neue Modell ist in etwa so breit und so hoch wie die Nintendo Switch-Konsole, besitzt aber einen größeren 7-Zoll-OLED-Bildschirm mit intensiver Farbdarstellung und hohem Kontrast.", "Maße: 10x20 : Gewicht: 10kg", "Konsole"));
+			repository.save(new Product("Nintendo Switch", "$", 5.99, "Das neue Modell ist in etwa so breit und so hoch wie die Nintendo Switch-Konsole, besitzt aber einen größeren 7-Zoll-OLED-Bildschirm mit intensiver Farbdarstellung und hohem Kontrast.", "Maße: 10x20 : Gewicht: 10kg", "Konsole"));
 			userRepository.save(new User( "admin", "password", "Admin", "0", "Hans","Müller"));
 			userRepository.save(new User( "user1", "1", "Standard Customer", "0", "Mahir", "Berkcan"));
 			userRepository.save(new User( "user2", "1", "Premium Customer", "10", "Tim", "Grunwald"));
 			addressRepository.save(new Address(4L,"Dr.",  "Grau", "Peter", "Hohenbacher Strasse", 34, 73261, "Karlsruhe"));
-			detailsRepository.save(new Details(2L, "Die Tankstellen Schreck ist zurück!!!", "Er überfällt Tankstellen, wie eh und je", "Playstation 5"));
-			detailsRepository.save(new Details(2L, "Die Reise des Mahirs", "Er reist durch eine Wüste", "Playstation 5"));
-			detailsRepository.save(new Details(3L, "Alibabas Reisen Teil 10", "Er reist weiter", "Nintendo Switch"));
+			detailsRepository.save(new Details(2L, "Maße", "10x5x20 cm", "Playstation 5"));
+			detailsRepository.save(new Details(2L, "Farbe", "violett", "Playstation 5"));
+			detailsRepository.save(new Details(3L, "Gewicht", "2 Tonnen", "Nintendo Switch"));
 		};
 	}
 }
